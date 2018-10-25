8f608f725b219747b1c884ce7d5b2bdcca15a5b7
Osman Ali <okali@uci.edu>
2018-10-25 01:46:15 -0700

    Add index and color to item schema

--- models/item.js
+++ models/item.js
@@ -3,6 +3,8 @@ const Schema = mongoose.Schema;
 
 const itemSchema = new Schema({
     title: { type: String, required: true },
+    index: { type: Number, required: true },
+    color: { type: String, require: true },
     details: String,
     comments: Array
 });
