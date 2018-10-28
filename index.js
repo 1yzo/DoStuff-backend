3358a8ede2df7a8d96697915314df20c3108b16d
Osman Ali <okali@uci.edu>
2018-10-28 01:11:29 -0700

    Remove dead code

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const bodyParser = require('body-parser');
 const app = new express();
 
 const boards = require('./routes/boards');
-const items = require('./routes/items');
 
 app.use(cors());
 app.use(bodyParser.json());
 
 require('./mongo').connect();
 app.use('/boards', boards);
-app.use('/items', items);
 
 const PORT = 3000;
 app.listen(PORT, () => {
