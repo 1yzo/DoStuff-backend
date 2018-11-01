186d5cd3ea025972ab4c5c63188e2e164d4101f9
Osman Ali <okali@uci.edu>
2018-11-01 14:33:39 -0700

    Make endpoint for getting open graph info

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const htmlparser = require('htmlparser');
+const fetch = require('node-fetch');
 const app = new express();
 
 const boards = require('./routes/boards');
@@ -10,6 +12,31 @@ app.use(bodyParser.json());
 
 require('./mongo').connect();
 app.use('/boards', boards);
+
+// Return Open Graph info to create link previews
+app.post('/api/link_preview', (req, res) => {
+    fetch(req.query.link)
+        .then(response => response.text())
+        .then(body => {
+            const handler = new htmlparser.DefaultHandler((error, dom) => {
+                if (error) {
+                    res.status(500).send(error);
+                }
+                const head = dom.find(element => element.name === 'html').children.find(element => element.name === 'head');
+                const metaTags = head.children
+                    .filter(element => element.name === 'meta')
+                    .filter(element => element.attribs.property && element.attribs.property.includes('og'));
+                const linkPreview = metaTags.reduce((acc, curr) => ({
+                    ...acc,
+                    [curr.attribs.property]: curr.attribs.content
+                }), {});
+
+                res.json(linkPreview);
+            }, { verbose: false });
+            const parser = new htmlparser.Parser(handler);
+            parser.parseComplete(body);
+        });
+})
 
 const PORT = 3000;
 app.listen(PORT, () => {
